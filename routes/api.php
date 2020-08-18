4d5b7af89fd139d9292f9c38afeabd88e0101f2b
edddduardo <173631@ids.upchiapas.edu.mx>
2020-08-18 00:37:48 -0500

    Modificaciones en las rutas, V1

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::prefix('v1')->group(function () {
+
     // Rutas libres.
     Route::namespace('Api')->group(function (){
         Route::get('grupos','GrupoController@index');
@@ -25,29 +27,31 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     });
 
     Route::post('register', "PassportController@register");
-    
     Route::post('login', "PassportController@login");
+    Route::post('logout', "PassportController@logout");
+    Route::post('info_user','PassportController@info_user');
+    Route::post('mail_form','MailController@sendMail');
 
     //Rutas protegidas
     Route::group(['middleware'=>'auth:api'], function(){
-
-        Route::post('logout', "PassportController@logout");
+        Route::namespace('Api')->group(function (){
         //Prods
-        Route::get('marca/{slug}','Api\BrandController@products');
+        Route::get('marca/{slug}','BrandController@products');
         // User
-        Route::post('zonas_activas','Api\UserController@zonasActivas');
-        Route::post('info_user','PassportController@info_user');
-        // Ubications
-        Route::post('ubication','Api\UbicationController@store');
-        Route::put('ubication/{id}','Api\UbicationController@update');
-        Route::delete('ubication/{id}','Api\UbicationController@destroy');
-        Route::get('ubication/{id}','Api\UbicationController@show');
-        //Images
-        Route::post('add_image','Api\ProductImageController@store');
-        Route::delete('delete_image/{id}','Api\ProductImageController@delete');
-        // Zonas
-        Route::get('zona/{slug}','Api\ZonaController@tiendas');
-        Route::get('zonas','Api\ZonaController@index');
+        Route::post('zonas_activas','UserController@zonasActivas');
 
-        Route::post('mail_form','MailController@sendMail');
+        // Ubications
+        Route::post('ubication','UbicationController@store');
+        Route::put('ubication/{id}','UbicationController@update');
+        Route::delete('ubication/{id}','UbicationController@destroy');
+        Route::get('ubication/{id}','UbicationController@show');
+
+        //Images
+        Route::post('add_image','ProductImageController@store');
+        Route::delete('delete_image/{id}','ProductImageController@delete');
+        // Zonas
+        Route::get('zona/{slug}','ZonaController@tiendas');
+        Route::get('zonas','ZonaController@index');
+        });
     });
+});
