ea6bb7bb3f28805c6ce2ccb00c15ea1ae999b7b4
Jesús Alberto GONZALEZ GUTIERREZ <163174@ids.upchiapas.edu.mx>
2020-08-24 10:00:11 -0500

    latest

--- database/seeds/CadenaTableSeeder.php
+++ database/seeds/CadenaTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Action;
+
 class CadenaTableSeeder extends Seeder
 {
     /**
@@ -14,5 +15,14 @@ class CadenaTableSeeder extends Seeder
         $cadena = new \App\Models\Chain();
         $cadena->nombre = 'Nueva';
         $cadena->save();
+
+
+        //despues cambiar
+        Action::create(["accion"=>"Acomoda"]);
+        Action::create(["accion"=>"Limpieza"]);
+        Action::create(["accion"=>"Ajuste de inventario"]);
+        Action::create(["accion"=>"Inventario"]);
+        Action::create(["accion"=>"Envío a merma"]);
+        Action::create(["accion"=>"Relleno de anaquel"]);
     }
 }
