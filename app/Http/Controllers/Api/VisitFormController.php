ee3e9e5adb79964a605be81be721a7e5591df16a
edddduardo <173631@ids.upchiapas.edu.mx>
2020-08-26 17:34:00 -0500

    visit modified

--- app/Http/Controllers/Api/VisitFormController.php
+++ app/Http/Controllers/Api/VisitFormController.php
@@ -13,16 +13,19 @@ class VisitFormController extends Controller
     {   
         try{
             $data = $request->all()['data'];
-            $VisitFormData = ["product_id"=>$data["producto_id"],"visita_id"=>$data["visita_id"],"exhibido"=>$data["exhibido"],
-            "agotado"=>$data["agotado"],"cumple_panorama"=>$data["cumple_panorama"],"tiene_promocion"=>$data["tiene_promocion"],
-            "cuenta_con_exhibicion_adicional"=>$data["cuenta_con_exhibicion_adicional"],"frentes"=>$data["frentes"],
-            "inventario"=>$data["inventario"],"en_transito"=>$data["en_transito"],"fecha_caducidad"=>$data["fecha_caducidad"],
-            "precio_piso_venta"=>$data["precio_piso_venta"],"comentarios"=>$data["comentarios"],"tiene_promocion_texto"=>$data["tiene_promocion_texto"],
-            "exhibicion_adicional_text"=>$data["exhibicion_adicional_text"]];
-            $VisitForm = VisitForm::create($VisitFormData);
-            return response()->json(['visita_id'=>$data["visita_id"] ],201);
-            // $user = auth()->user();
-            // $name = "report.pdf";
+            if(!$data["final"]){
+                $VisitFormData = ["product_id"=>$data["producto_id"],"visita_id"=>$data["visita_id"],"exhibido"=>$data["exhibido"],
+                "agotado"=>$data["agotado"],"cumple_panorama"=>$data["cumple_panorama"],"tiene_promocion"=>$data["tiene_promocion"],
+                "cuenta_con_exhibicion_adicional"=>$data["cuenta_con_exhibicion_adicional"],"frentes"=>$data["frentes"],
+                "inventario"=>$data["inventario"],"en_transito"=>$data["en_transito"],"fecha_caducidad"=>$data["fecha_caducidad"],
+                "precio_piso_venta"=>$data["precio_piso_venta"],"comentarios"=>$data["comentarios"],"tiene_promocion_texto"=>$data["tiene_promocion_texto"],
+                "exhibicion_adicional_text"=>$data["exhibicion_adicional_text"]];
+                $VisitForm = VisitForm::create($VisitFormData);
+                return response()->json(['visita_id'=>$data["visita_id"]],201);
+            }
+
+            $user = auth()->user();
+            $name = "report.pdf";
             // view()->share('data',$results);
             // $pdf = PDF::loadView('PDF.pdf');
             // $pdf->save($name);
